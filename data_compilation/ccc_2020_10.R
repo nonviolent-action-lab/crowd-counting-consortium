699c2404753984398dbff2811175792ccb66f8f7
Nonviolent Action Lab <68960256+nonviolent-action-lab@users.noreply.github.com>
2021-12-15 14:30:08 -0500

    Weekly data update

--- data_compilation/ccc_2020_10.R
+++ data_compilation/ccc_2020_10.R
@@ -2,22 +2,10 @@ source("~/nval/ccc/r/ccc_scrubber.r")
 
 dat <- read_excel("data_raw/Crowd Estimates October 2020.xlsx", sheet = "Tally")
 
-dat_1 <- dat %>%
+dat <- dat %>%
   select(-starts_with("...")) %>%
   rename(ClaimType = `Pro(2)/Anti(1)`, Misc = Misc.) %>%
   mutate(Date = datescrub(Date),
          Final = 0)
-
-dat <- read_excel("data_raw/Crowd Estimates October 2020.xlsx", sheet = "WomensMarch")
-
-dat_2 <- dat %>%
-  select(-starts_with("...")) %>%
-  rename(ClaimType = `Pro(2)/Anti(1)`, Misc = Misc.) %>%
-  mutate(Date = datescrub(Date),
-         MacroEvent = "20201017-womensmarch",
-         Final = 0)
-
-# need to use data.table here to resolve issue with inconsistent col types
-dat <- data.table::rbindlist(list(dat_1, dat_2), fill = TRUE)
               
 write.csv(dat, "data_clean/ccc_2020_10.csv", row.names = FALSE)
